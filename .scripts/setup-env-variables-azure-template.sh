9cc7215331b28fc61c8e282b1ea77accc7bb6831
felipmiguel <fmiguel@microsoft.com>
2023-01-15 23:43:12 +0100

    Github Action passwordless + Java 17

--- .scripts/setup-env-variables-azure-template.sh
+++ .scripts/setup-env-variables-azure-template.sh
@@ -2,11 +2,11 @@
 
 # ==== Resource Group ====
 export SUBSCRIPTION=subscription-id # customize this
-export RESOURCE_GROUP=resource-group-name # customize this
-export REGION=westus2
+export RESOURCE_GROUP=rg-spring-petclinic-276 # customize this
+export REGION=eastus
 
 # ==== Service and App Instances ====
-export SPRING_CLOUD_SERVICE=azure-spring-cloud-name # customize this
+export SPRING_CLOUD_SERVICE=fmiguel-spring-petclinic # customize this
 export API_GATEWAY=api-gateway
 export ADMIN_SERVER=admin-server
 export CUSTOMERS_SERVICE=customers-service
@@ -14,11 +14,12 @@ export VETS_SERVICE=vets-service
 export VISITS_SERVICE=visits-service
 
 # ==== JARS ====
-export API_GATEWAY_JAR=spring-petclinic-api-gateway/target/spring-petclinic-api-gateway-2.5.1.jar
-export ADMIN_SERVER_JAR=spring-petclinic-admin-server/target/spring-petclinic-admin-server-2.5.1.jar
-export CUSTOMERS_SERVICE_JAR=spring-petclinic-customers-service/target/spring-petclinic-customers-service-2.5.1.jar
-export VETS_SERVICE_JAR=spring-petclinic-vets-service/target/spring-petclinic-vets-service-2.5.1.jar
-export VISITS_SERVICE_JAR=spring-petclinic-visits-service/target/spring-petclinic-visits-service-2.5.1.jar
+springboot_version=3.0.1
+export API_GATEWAY_JAR=spring-petclinic-api-gateway/target/spring-petclinic-api-gateway-${springboot_version}.jar
+export ADMIN_SERVER_JAR=spring-petclinic-admin-server/target/spring-petclinic-admin-server-${springboot_version}.jar
+export CUSTOMERS_SERVICE_JAR=spring-petclinic-customers-service/target/spring-petclinic-customers-service-${springboot_version}.jar
+export VETS_SERVICE_JAR=spring-petclinic-vets-service/target/spring-petclinic-vets-service-${springboot_version}.jar
+export VISITS_SERVICE_JAR=spring-petclinic-visits-service/target/spring-petclinic-visits-service-${springboot_version}.jar
 
 # ==== MYSQL INFO ====
 export MYSQL_SERVER_NAME=mysql-servername # customize this
